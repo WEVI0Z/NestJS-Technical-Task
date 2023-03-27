7299ce75d68a3e26e32f44e7669d80509d0ac83e
Wevioz <wevioz@mail.ru>
2023-03-27 13:47:06 +0300

    set up the swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,26 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+
+function connectSwagger(app) {
+  const config = new DocumentBuilder()
+    .setTitle('NestJS Technical Task')
+    .setDescription('NestJS Technical Task API description')
+    .setVersion('0.0.1')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config, {
+   include: [AppModule],
+  });
+  
+  SwaggerModule.setup('api', app, document);
+}
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  connectSwagger(app);
+
   await app.listen(3001);
 }
 bootstrap();
